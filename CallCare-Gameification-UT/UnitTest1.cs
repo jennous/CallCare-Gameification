83dba99d53e0075f735b257993565fc34b2a8114
dipesh patel <dipesh@pateltechnologies.co.uk>
2025-01-24 12:15:29 +0000

    Add project files.

---
+++ CallCare-Gameification-UT/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CallCare_Gameification_UT
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
