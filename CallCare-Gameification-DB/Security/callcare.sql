d503f099a8115e03f6a961311c12aaa4b115d5c7
dipesh patel <dipesh@pateltechnologies.co.uk>
2025-01-24 12:50:18 +0000

    added db project

---
+++ CallCare-Gameification-DB/Security/callcare.sql
@@ -0,0 +1,2 @@
+﻿CREATE USER [callcare] FOR LOGIN [callcare];
+
---
+++ CallCare-Gameification-DB/Security/callcare_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [callcare]
+    WITH PASSWORD = N'@hmYbqbxh{rv{8cqupsb+qvWmsFT7_&#$!~<lv8w1zsn ed|', SID = 0xABB0C8021036B0488DDAC2979E07E87C, DEFAULT_DATABASE = [CallCare-Gameification], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
+
